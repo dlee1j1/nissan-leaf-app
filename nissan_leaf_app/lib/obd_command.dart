9b7225c0867ee898586c60d198d20e0e6a6350bc
dlee1j1 <53192782+dlee1j1@users.noreply.github.com>
2025-02-11 15:24:22 -0800

    added rest of commands from reference implementation

--- nissan_leaf_app/lib/obd_command.dart
+++ nissan_leaf_app/lib/obd_command.dart
@@ -50,10 +50,42 @@ abstract class OBDCommand {
   static final OBDCommand lbc = _LBCCommand();
   static final OBDCommand powerSwitch = _PowerSwitchCommand();
   static final OBDCommand gearPosition = _GearPositionCommand();
-  static final OBDCommand battery12v = _12VBatteryCommand();
   static final OBDCommand odometer = _OdometerCommand();
+  static final OBDCommand battery12v = _12VBatteryCommand();
+  static final OBDCommand bat12vCurrent = _12VBatteryCurrentCommand();
+  static final OBDCommand quickCharges = _QuickChargesCommand();
+  static final OBDCommand l1l2Charges = _L1L2ChargesCommand();
+  static final OBDCommand ambientTemp = _AmbientTempCommand();
+  static final OBDCommand estimatedAcPower = _EstimatedAcPowerCommand();
+  static final OBDCommand estimatedPtcPower = _EstimatedPtcPowerCommand();
+  static final OBDCommand auxPower = _AuxPowerCommand();
+  static final OBDCommand acPower = _AcPowerCommand();
+  static final OBDCommand plugState = _PlugStateCommand();
+  static final OBDCommand chargeMode = _ChargeModeCommand();
+  static final OBDCommand rpm = _RPMCommand();
+  static final OBDCommand obcOutPower = _ObcOutPowerCommand();
+  static final OBDCommand motorPower = _MotorPowerCommand();
+  static final OBDCommand speed = _SpeedCommand();
+  static final OBDCommand acOn = _AcOnCommand();
+  static final OBDCommand rearHeater = _RearHeaterCommand();
+  static final OBDCommand ecoMode = _EcoModeCommand();
+  static final OBDCommand ePedalMode = _EPedalModeCommand();
+  static final OBDCommand tpFr = _TpFrCommand();
+  static final OBDCommand tpFl = _TpFlCommand();
+  static final OBDCommand tpRr = _TpRrCommand();
+  static final OBDCommand tpRl = _TpRlCommand();
+  static final OBDCommand rangeRemaining = _RangeRemainingCommand();
+
+
   static final _registry = [
-    lbc, powerSwitch,gearPosition, battery12v, odometer,
+    lbc, rangeRemaining, odometer, l1l2Charges, quickCharges, plugState, chargeMode,
+    tpFr, tpFl, tpRr, tpRl, 
+    battery12v, bat12vCurrent,
+    gearPosition, speed, rpm, motorPower, ecoMode, ePedalMode,   
+    ambientTemp, estimatedAcPower,
+    estimatedPtcPower, auxPower, acPower, 
+    obcOutPower, acOn, rearHeater,  
+    powerSwitch,
   ];  
   static List<OBDCommand> getAllCommands() => _registry;
 
@@ -246,6 +278,408 @@ class _OdometerCommand extends OBDCommand {
   Map<String, dynamic> decode(List<int> data) {
     return {
       'odometer': extractInt(data, 3, 6),
+    };
+  }
+}
+
+class _12VBatteryCurrentCommand extends OBDCommand {
+  _12VBatteryCurrentCommand()
+      : super(
+          name: 'bat_12v_current',
+          description: '12V battery current',
+          command: '03221183',
+          header: '797',
+        );
+
+  @override
+  Map<String, dynamic> decode(List<int> data) {
+    return {
+      'bat_12v_current': extractInt(data, 3, 5) / 256,
+    };
+  }
+}
+
+class _QuickChargesCommand extends OBDCommand {
+  _QuickChargesCommand()
+      : super(
+          name: 'quick_charges',
+          description: 'Number of quick charges',
+          command: '03221203',
+          header: '797',
+        );
+
+  @override
+  Map<String, dynamic> decode(List<int> data) {
+    return {
+      'quick_charges': extractInt(data, 3, 5),
+    };
+  }
+}
+
+class _L1L2ChargesCommand extends OBDCommand {
+  _L1L2ChargesCommand()
+      : super(
+          name: 'l1_l2_charges',
+          description: 'Number of L1/L2 charges',
+          command: '03221205',
+          header: '797',
+        );
+
+  @override
+  Map<String, dynamic> decode(List<int> data) {
+    return {
+      'l1_l2_charges': extractInt(data, 3, 5),
+    };
+  }
+}
+
+class _AmbientTempCommand extends OBDCommand {
+  _AmbientTempCommand()
+      : super(
+          name: 'ambient_temp',
+          description: 'Ambient temperature',
+          command: '0322115d',
+          header: '797',
+        );
+
+  @override
+  Map<String, dynamic> decode(List<int> data) {
+    return {
+      'ambient_temp': data[3] / 2 - 40,
+    };
+  }
+}
+
+class _EstimatedAcPowerCommand extends OBDCommand {
+  _EstimatedAcPowerCommand()
+      : super(
+          name: 'estimated_ac_power',
+          description: 'Estimated AC system power',
+          command: '03221261',
+          header: '797',
+        );
+
+  @override
+  Map<String, dynamic> decode(List<int> data) {
+    return {
+      'estimated_ac_power': data[3] * 50,
+    };
+  }
+}
+
+class _EstimatedPtcPowerCommand extends OBDCommand {
+  _EstimatedPtcPowerCommand()
+      : super(
+          name: 'estimated_ptc_power',
+          description: 'Est Power Temp Coef (PTC - i.e., heater) consumption',
+          command: '03221262',
+          header: '797',
+        );
+
+  @override
+  Map<String, dynamic> decode(List<int> data) {
+    return {
+      'estimated_ptc_power': data[3] * 250,
+    };
+  }
+}
+
+class _AuxPowerCommand extends OBDCommand {
+  _AuxPowerCommand()
+      : super(
+          name: 'aux_power',
+          description: 'Auxiliary equipment power',
+          command: '03221152',
+          header: '797',
+        );
+
+  @override
+  Map<String, dynamic> decode(List<int> data) {
+    return {
+      'aux_power': data[3] * 100,
+    };
+  }
+}
+
+class _AcPowerCommand extends OBDCommand {
+  _AcPowerCommand()
+      : super(
+          name: 'ac_power',
+          description: 'AC system power',
+          command: '03221151',
+          header: '797',
+        );
+
+  @override
+  Map<String, dynamic> decode(List<int> data) {
+    return {
+      'ac_power': data[3] * 250,
+    };
+  }
+}
+
+class _PlugStateCommand extends OBDCommand {
+  _PlugStateCommand()
+      : super(
+          name: 'plug_state',
+          description: 'Plug state of J1772 socket',
+          command: '03221234',
+          header: '797',
+        );
+
+  @override
+  Map<String, dynamic> decode(List<int> data) {
+    String state;
+    switch (data[3]) {
+      case 0: state = "Not plugged"; break;
+      case 1: state = "Partial plugged"; break;
+      case 2: state = "Plugged"; break;
+      default: state = "Unknown";
+    }
+    return {'plug_state': state};
+  }
+}
+
+class _ChargeModeCommand extends OBDCommand {
+  _ChargeModeCommand()
+      : super(
+          name: 'charge_mode',
+          description: 'Charging mode',
+          command: '0322114e',
+          header: '797',
+        );
+
+  @override
+  Map<String, dynamic> decode(List<int> data) {
+    String mode;
+    switch (data[3]) {
+      case 0: mode = "Not charging"; break;
+      case 1: mode = "L1 charging"; break;
+      case 2: mode = "L2 charging"; break;
+      case 3: mode = "L3 charging"; break;
+      default: mode = "Unknown";
+    }
+    return {'charge_mode': mode};
+  }
+}
+
+class _RPMCommand extends OBDCommand {
+  _RPMCommand()
+      : super(
+          name: 'rpm',
+          description: 'Motor RPM',
+          command: '03221255',
+          header: '797',
+        );
+
+  @override
+  Map<String, dynamic> decode(List<int> data) {
+    return {
+      'rpm': extractInt(data, 3, 5),
+    };
+  }
+}
+
+class _ObcOutPowerCommand extends OBDCommand {
+  _ObcOutPowerCommand()
+      : super(
+          name: 'obc_out_power',
+          description: 'On-board charger output power',
+          command: '03221236',
+          header: '797',
+        );
+
+  @override
+  Map<String, dynamic> decode(List<int> data) {
+    return {
+      'obc_out_power': extractInt(data, 3, 5) * 100,
+    };
+  }
+}
+
+class _MotorPowerCommand extends OBDCommand {
+  _MotorPowerCommand()
+      : super(
+          name: 'motor_power',
+          description: 'Traction motor power',
+          command: '03221146',
+          header: '797',
+        );
+
+  @override
+  Map<String, dynamic> decode(List<int> data) {
+    return {
+      'motor_power': extractInt(data, 3, 5) * 40,
+    };
+  }
+}
+
+class _SpeedCommand extends OBDCommand {
+  _SpeedCommand()
+      : super(
+          name: 'speed',
+          description: 'Vehicle speed',
+          command: '0322121a',
+          header: '797',
+        );
+
+  @override
+  Map<String, dynamic> decode(List<int> data) {
+    return {
+      'speed': extractInt(data, 3, 5) / 10,
+    };
+  }
+}
+
+class _AcOnCommand extends OBDCommand {
+  _AcOnCommand()
+      : super(
+          name: 'ac_on', 
+          description: 'AC status',
+          command: '03221106',
+          header: '797',
+        );
+
+  @override
+  Map<String, dynamic> decode(List<int> data) {
+    return {
+      'ac_on': data[3] == 0x01,
+    };
+  }
+}
+
+class _RearHeaterCommand extends OBDCommand {
+  _RearHeaterCommand()
+      : super(
+          name: 'rear_heater',
+          description: 'Rear heater status',
+          command: '0322110f',
+          header: '797',
+        );
+
+  @override
+  Map<String, dynamic> decode(List<int> data) {
+    return {
+      'rear_heater': data[3] == 0xA2,
+    };
+  }
+}
+
+class _EcoModeCommand extends OBDCommand {
+  _EcoModeCommand()
+      : super(
+          name: 'eco_mode',
+          description: 'ECO mode status',
+          command: '03221318',
+          header: '797',
+        );
+
+  @override
+  Map<String, dynamic> decode(List<int> data) {
+    return {
+      'eco_mode': data[3] == 0x10 || data[3] == 0x11,
+    };
+  }
+}
+
+class _EPedalModeCommand extends OBDCommand {
+  _EPedalModeCommand()
+      : super(
+          name: 'e_pedal_mode',
+          description: 'e-Pedal mode status',
+          command: '0322131A',
+          header: '797',
+        );
+
+  @override
+  Map<String, dynamic> decode(List<int> data) {
+    return {
+      'e_pedal_mode': data[3] == 0x04,
+    };
+  }
+}
+
+class _TpFrCommand extends OBDCommand {
+  _TpFrCommand()
+      : super(
+          name: 'tp_fr',
+          description: 'Tyre pressure front right',
+          command: '03220e25',
+          header: '743',
+        );
+
+  @override
+  Map<String, dynamic> decode(List<int> data) {
+    return {
+      'tp_fr': data[3] * 1.7236894,
+    };
+  }
+}
+
+class _TpFlCommand extends OBDCommand {
+  _TpFlCommand()
+      : super(
+          name: 'tp_fl',
+          description: 'Tyre pressure front left',
+          command: '03220e26',
+          header: '743',
+        );
+
+  @override
+  Map<String, dynamic> decode(List<int> data) {
+    return {
+      'tp_fl': data[3] * 1.7236894,
+    };
+  }
+}
+
+class _TpRrCommand extends OBDCommand {
+  _TpRrCommand()
+      : super(
+          name: 'tp_rr',
+          description: 'Tyre pressure rear right',
+          command: '03220e27',
+          header: '743',
+        );
+
+  @override
+  Map<String, dynamic> decode(List<int> data) {
+    return {
+      'tp_rr': data[3] * 1.7236894,
+    };
+  }
+}
+
+class _TpRlCommand extends OBDCommand {
+  _TpRlCommand()
+      : super(
+          name: 'tp_rl',
+          description: 'Tyre pressure rear left',
+          command: '03220e28',
+          header: '743',
+        );
+
+  @override
+  Map<String, dynamic> decode(List<int> data) {
+    return {
+      'tp_rl': data[3] * 1.7236894,
+    };
+  }
+}
+
+class _RangeRemainingCommand extends OBDCommand {
+  _RangeRemainingCommand()
+      : super(
+          name: 'range_remaining',
+          description: 'Remaining range (km)',
+          command: '03220e24',
+          header: '743',
+        );
+
+  @override
+  Map<String, dynamic> decode(List<int> data) {
+    return {
+      'range_remaining': extractInt(data, 3, 5) / 10,
     };
   }
 }
